a9f3f5f50bf71224cb3debe5b50209c94d6659f2
Christian Haudum <christian@rjdj.me>
2014-05-05 10:44:35 +0200

    theme wip

---
+++ setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8; -*-
+#
+# Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+# license agreements.  See the NOTICE file distributed with this work for
+# additional information regarding copyright ownership.  Crate licenses
+# this file to you under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.  You may
+# obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# However, if you have executed another commercial license agreement
+# with Crate these terms will supersede the license and you may use the
+# software solely pursuant to the terms of the relevant commercial agreement.
+
+
+from setuptools import setup, find_packages
+
+
+setup(name='crate-rtd',
+      version='0.1',
+      description='CRATE Read The Docs',
+      long_description='A Sphinx theme for RTD-Documentation',
+      classifiers=[
+          "Programming Language :: JavaScript",
+          "Programming Language :: Python",
+      ],
+      author='CRATE Technology GmbH',
+      author_email='office@crate.io',
+      url='https://github.com/crate/crate-rtd',
+      keywords='',
+      license='Apache License 2.0',
+      packages=find_packages('src'),
+      package_dir={'':'src'},
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Sphinx',
+          'sphinxcontrib-plantuml',
+      ],
+)
