89e3fc5932483cc3aa0938f372f3175415166cd8
Philipp Bogensberger <bogensberger@lovelysystems.com>
2014-05-06 11:25:38 +0200

    added configuration for crate-server and crate-python documentation

---
+++ src/crate/rtd_theme/conf/__init__.py
@@ -0,0 +1,33 @@
+import crate.rtd_theme
+
+copyright = u'2014, CRATE Technology GmbH'
+
+html_favicon = 'out/html/_static/img/favicon.ico'
+logo = 'img/logo_crate.png'
+
+# The suffix of source filenames.
+source_suffix = '.txt'
+exclude_patterns = ['requirements.txt']
+
+# The master toctree document.
+master_doc = 'index'
+exclude_trees = ['pyenv', 'tmp', 'out', 'parts', 'clients', 'eggs']
+extensions = ['sphinx.ext.autodoc']
+
+# Configure the theme
+html_theme = 'crate'
+html_theme_path = crate.rtd_theme.get_html_theme_path()
+nitpicky = True
+html_show_sourcelink = False
+html_sidebars = {'**': ['sidebar.html', 'sourcelink.html']}
+html_theme_options = {
+    # HTML navbar class (Default: "navbar") to attach to <div> element.
+    # For black navbar, do "navbar navbar-inverse"
+    'navbar_class': 'navbar navbar-inverse',
+
+    # Fix navigation bar to top of page?
+    # Values: "true" (default) or "false"
+    'navbar_fixed_top': 'true',
+
+    'globaltoc_includehidden': 'true',
+}
---
+++ src/crate/rtd_theme/conf/crate_server.py
@@ -0,0 +1,3 @@
+from crate.rtd_theme.conf import *
+
+project = u'Crate Data'
