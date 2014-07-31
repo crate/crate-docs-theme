426191393430f6d2180b5f4c2029df56b1929913
Christian Haudum <christian.haudum@gmail.com>
2014-07-31 13:48:40 +0200

    fixed internal page links

---
+++ src/crate/theme/rtd/crate/static/js/theme.js
@@ -0,0 +1,35 @@
+$(function() {
+  var path = window.location.pathname;
+
+  var scrollToSection = function(name) {
+    var posTop = $('#'+name).offset().top;
+    var offset = Math.max(posTop-60, 0);
+    window.setTimeout(function(){
+      $(window).scrollTop(offset);
+    }, 100);
+  };
+
+  $('table').attr("border","0").addClass('table');
+  $('.search').addClass('list-unstyled');
+  $('.toctree ul').addClass('nav');
+  $('.localtoc ul').addClass('nav');
+  $('.toctree li a').click(function(e){
+    var urlParts = this.href.split('#');
+    if (urlParts[0].indexOf(path, urlParts[0].length-path.length) > -1 && urlParts.length == 2) {
+      scrollToSection(urlParts[1]);
+    }
+  });
+
+  $('body').scrollspy({
+    'target': '.toctree',
+    'offset': 51
+  });
+
+  (function(){
+    var urlParts = window.location.hash.split('#');
+    if (urlParts.length == 2) {
+      scrollToSection(urlParts[1]);
+    }
+  }());
+
+});
