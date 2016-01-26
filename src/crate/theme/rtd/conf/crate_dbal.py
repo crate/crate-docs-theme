77085e45d8e6b1f4b5c7e998e645b4e1108eafe9
Chris Ward <chriswhward@gmail.com>
2016-01-26 20:56:03 +0100

    Adding Primary domains

--- src/crate/theme/rtd/conf/crate_dbal.py
+++ src/crate/theme/rtd/conf/crate_dbal.py
@@ -22,6 +22,8 @@
 from crate.theme.rtd.conf import *
 
 project = u'Crate DBAL'
+primary_domain = 'Crate.DBAL'
+
 html_theme_options.update({
     'canonical_url_path': 'docs/projects/crate-dbal/',
     'tracking_project': 'crate-dbal',
