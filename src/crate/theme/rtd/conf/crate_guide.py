e2731405ae513a6333155ff4b0d90f62a82bbe7e
Naomi Slater <nslater@tumbolia.org>
2017-08-01 19:42:13 +0200

    canonical url debug

--- src/crate/theme/rtd/conf/crate_guide.py
+++ src/crate/theme/rtd/conf/crate_guide.py
@@ -24,6 +24,7 @@ from crate.theme.rtd.conf import *
 project = u'Crate Guide'
 
 html_theme_options.update({
+    'canonical_url': 'http://crate.io/docs/crate/guide/',
     'canonical_url_path': 'docs/crate/guide/',
     'tracking_project': 'guide',
 })
