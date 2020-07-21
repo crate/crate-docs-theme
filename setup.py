77b9e81ac5d98464f539a8b8a4ad56b040d04637
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-07-21 09:00:02 +0200

    Downgraded Sphinx dependency to 1.8.5 (matches new RTD default)

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='crate-docs-theme',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'Sphinx==3.1.2',
+          'Sphinx==1.8.5',
           'sphinxcontrib-plantuml==0.14',
           'sphinx_sitemap==0.3.1',
       ],
