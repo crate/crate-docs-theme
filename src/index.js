a0777ba59e31adf296868934c47b56e554d74659
Andreas Motl <andreas.motl@crate.io>
2021-04-13 20:59:18 +0200

    Improve bundling of custom JavaScript and CSS assets

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * This is a surrogate for satisfying webpack.
+ *
+ * In order to build the resources, invoke::
+ *
+ *  npx webpack --mode=development
+ *
+ * or
+ *
+ *  npx webpack --mode=production
+ *
+ **/
+import "./crate/theme/rtd/crate/static/css/index.css";
+import "./crate/theme/rtd/crate/static/js/index";
