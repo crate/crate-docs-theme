4b37ec0673dfbbdda394f0aa3287496fad96c4d9
Andreas Motl <andreas.motl@crate.io>
2021-04-19 13:31:16 +0200

    Remove Fontawesome

--- src/crate/theme/rtd/crate/static/js/index.js
+++ src/crate/theme/rtd/crate/static/js/index.js
@@ -2,21 +2,5 @@ import "jquery";
 import "jquery.cookie";
 import "sticky-sidebar";
 
-window.FontAwesomeCdnConfig = {
-    autoA11y: {
-        enabled: false
-    },
-    asyncLoading: {
-        enabled: true
-    },
-    reporting: {
-        enabled: false
-    },
-    useUrl: "use.fontawesome.com",
-    faCdnUrl: "https://cdn.fontawesome.com",
-    code: "34da4ffc93"
-};
-import "fontawesome";
-
 import "./crate";
 import "./custom";
