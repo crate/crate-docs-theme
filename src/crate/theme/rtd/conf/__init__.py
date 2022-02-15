f5519d4c439f9bb20305c542bad3d2f8b0c64a82
Matthias <matthias@zugastbeiverlierern.at>
2022-02-15 11:20:27 +0100

    Update src/crate/theme/rtd/conf/__init__.py
    Co-authored-by: Andreas Motl <andreas.motl@crate.io>

--- src/crate/theme/rtd/conf/__init__.py
+++ src/crate/theme/rtd/conf/__init__.py
@@ -60,7 +60,7 @@ html_theme_options = {
     "tracking_hubspot_id": environ.get("TRACKING_HUBSPOT_ID", ""),
     "tracking_project": "",
     # Google custom search engine
-    "google_search_api": environ.get("GOOGLE_SEARCH_API", ""),
+    "google_search_api": environ.get("GOOGLE_SEARCH_API_KEY", ""),
     "google_search_cx": environ.get("GOOGLE_SEARCH_CX", ""),
 
     # Can be used the query string of a resource URL for HTTP cache busting
