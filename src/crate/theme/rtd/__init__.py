3318b515fdfc15298c356132d020fd136a03f148
Brian Munkholm <brian.munkholm@crate.io>
2025-12-17 09:33:59 +0100

    minor refactor

--- src/crate/theme/rtd/__init__.py
+++ src/crate/theme/rtd/__init__.py
@@ -42,17 +42,11 @@ def get_html_theme_path():
 
 def get_html_static_path():
     """Return list of HTML static paths."""
-    current_dir = current_dir()
-    return [
-        os.path.join(current_dir, "crate", "static"),
-    ]
+    return [os.path.join(current_dir(), "crate", "static")]
 
 def get_html_template_path():
     """Return list of HTML template paths."""
-    current_dir = current_dir()
-    return [
-        os.path.join(current_dir, "crate"),
-    ]
+    return [os.path.join(current_dir(), "crate")]
 
 
 def _add_crate_navigation(app, pagename, templatename, context, doctree):
