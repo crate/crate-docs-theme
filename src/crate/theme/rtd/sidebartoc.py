e95789714478bbcda4cd0bc6a3e55aa6efc007b0
Kenneth Geisshirt <k@zigzak.net>
2026-07-03 16:19:36 +0200

    Add agent skills to the menu

--- src/crate/theme/rtd/sidebartoc.py
+++ src/crate/theme/rtd/sidebartoc.py
@@ -183,6 +183,7 @@ def _generate_crate_navigation_html(context):
     builder.add_project_nav_item('CrateDB: Crash CLI', 'CrateDB CLI', '/docs/crate/crash/')
     builder.add_project_nav_item('CrateDB Cloud: Croud CLI', 'Cloud CLI', '/docs/cloud/cli/')
     parts.append('<li class="navleft-item"><a class="external-link" target="_blank" href="https://cratedb-mcp.readthedocs.io/">CrateDB MCP</a></li>')
+    parts.append('<li class="navleft-item"><a class="external-link" target="_blank" href="https://github.com/crate/agent-skills/">CrateDB Agent Skills</a></li>')
     parts.append('<li class="navleft-item"><a class="external-link" target="_blank" href="https://cratedb-toolkit.readthedocs.io/">CrateDB Toolkit</a></li>')
 
     # Add Support and Community links section after a border
